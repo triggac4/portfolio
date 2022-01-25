c8e34674c12f0fbe7db0cd3012275ad01b0d9a7f
kene3055 <koffokansi@gmail.com>
2022-01-25 05:12:38 +0100

    nav still directing to /portfolio error

--- src/component/navigation-item.js
+++ src/component/navigation-item.js
@@ -1,23 +1,61 @@
 import React from "react";
-import {
-  Link, Route,Switch
-} from "react-router-dom";
-import Svg from './svg';
+import { Link, Route, Switch } from "react-router-dom";
+import Svg from "./svg";
 
-let navigationItem=props=>{
+let navigationItem = (props) => {
     return (
-    <div className="navigationItem ">
-      <Link className="navigationItem__link" onClick={props.close} to="/portfolio"><Svg className="navigationItem__svg" type="icon-home"/>Home</Link>
-      <Switch>
-        <Route exact path="/portfolio">
-          <a href="#about" onClick={props.close} className="navigationItem__link"><Svg className="navigationItem__svg" type="icon-user"/>about</a>
-          <a href="#resume" onClick={props.close} className="navigationItem__link"><Svg className="navigationItem__svg" type="icon-file-empty"/>resume</a>
-          <a href="#portfolio" onClick={props.close} className="navigationItem__link"><Svg className="navigationItem__svg" type="icon-briefcase"/>portfolio</a>
-          <a href="#contact" onClick={props.close} className="navigationItem__link"><Svg className="navigationItem__svg" type="icon-phone"/>contact</a>
-        </Route>
-      </Switch>
-    </div>
-);
-}
+        <div className="navigationItem ">
+            <Link className="navigationItem__link" onClick={props.close} to="/">
+                <Svg className="navigationItem__svg" type="icon-home" />
+                Home
+            </Link>
+            <Switch>
+                <Route exact path="/">
+                    <a
+                        href="#about"
+                        onClick={props.close}
+                        className="navigationItem__link"
+                    >
+                        <Svg className="navigationItem__svg" type="icon-user" />
+                        about
+                    </a>
+                    <a
+                        href="#resume"
+                        onClick={props.close}
+                        className="navigationItem__link"
+                    >
+                        <Svg
+                            className="navigationItem__svg"
+                            type="icon-file-empty"
+                        />
+                        resume
+                    </a>
+                    <a
+                        href="#portfolio"
+                        onClick={props.close}
+                        className="navigationItem__link"
+                    >
+                        <Svg
+                            className="navigationItem__svg"
+                            type="icon-briefcase"
+                        />
+                        portfolio
+                    </a>
+                    <a
+                        href="#contact"
+                        onClick={props.close}
+                        className="navigationItem__link"
+                    >
+                        <Svg
+                            className="navigationItem__svg"
+                            type="icon-phone"
+                        />
+                        contact
+                    </a>
+                </Route>
+            </Switch>
+        </div>
+    );
+};
 
 export default navigationItem;
