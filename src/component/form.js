e9606fe08c6799907539ac7cb7dd5bb075db76a7
kene3055 <koffokansi@gmail.com>
2022-08-07 13:12:17 +0100

    adding new portfolio files

--- src/component/form.js
+++ src/component/form.js
@@ -1,122 +1,85 @@
 import { useState, useRef } from "react";
 
 const Form = (props) => {
-    const [formValue, setFormValue] = useState({
-        name: "",
-        email: "",
-        subject: "",
-        body: "",
-    });
-    const [isLoading, setIsLoading] = useState(false);
-    const shouldSendRequest = useRef(true);
-    let onChange = (event) => {
-        setFormValue({ ...formValue, [event.target.name]: event.target.value });
-        console.log(formValue);
-    };
+  const [formValue, setFormValue] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    body: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  let onChange = (event) => {
+    setFormValue({ ...formValue, [event.target.name]: event.target.value });
+    console.log(formValue);
+  };
 
-    async function onSubmit(event) {
-        event.preventDefault();
-        let containsEmpty = false;
-        Object.keys(formValue).forEach((e) => {
-            if (!formValue[e]) {
-                containsEmpty = true;
-            }
-        });
-        if (!containsEmpty && shouldSendRequest.current) {
-            shouldSendRequest.current = false;
-            setIsLoading(true);
-            try {
-                await fetch(
-                    "https://mailer-3055.herokuapp.com/api/v1/contact",
-                    {
-                        method: "post",
-                        body: JSON.stringify(formValue),
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                    }
-                );
+  return (
+    <form
+      data-aos={"fade-left"}
+      action="mailto:koffokansi@gmail.com"
+      className="form"
+      method="post"
+      enctype="multipart/form-data"
+      name="EmailForm"
+    >
+      <div className="form__element">
+        <label htmlFor="name">Your name:</label>
+        <input
+          id="name"
+          value={formValue["Contact-Name"]}
+          name="Contact-Name"
+          className="form__input"
+          type="text"
+          required
+          onChange={onChange}
+        />
+      </div>
+      <div className="form__element">
+        <label htmlFor="email">Your email:</label>
+        <input
+          id="email"
+          name="Contact-Email"
+          value={formValue["Contact-Email"]}
+          className="form__input"
+          type="email"
+          required
+          onChange={onChange}
+        />
+      </div>
+      <div className="form__element form__span">
+        <label htmlFor="title">title:</label>
+        <input
+          id="title"
+          name="subject"
+          className="form__input"
+          type="text"
+          value={formValue.subject}
+          required
+          onChange={onChange}
+        />
+      </div>
 
-                setFormValue({
-                    name: "",
-                    email: "",
-                    subject: "",
-                    body: "",
-                });
-            } catch (e) {
-                console.log(e);
-            } finally {
-                shouldSendRequest.current = true;
-                setIsLoading(false);
-            }
-        }
-    }
-    return (
-        <form
-            data-aos={"fade-left"}
-            action="http://localhost:3000/api/v1/contact"
-            className="form"
-            method="post"
-        >
-            <div className="form__element">
-                <label htmlFor="name">Your name:</label>
-                <input
-                    id="name"
-                    value={formValue.name}
-                    name="name"
-                    className="form__input"
-                    type="text"
-                    required
-                    onChange={onChange}
-                />
-            </div>
-            <div className="form__element">
-                <label htmlFor="email">Your email:</label>
-                <input
-                    id="email"
-                    name="email"
-                    value={formValue.email}
-                    className="form__input"
-                    type="email"
-                    required
-                    onChange={onChange}
-                />
-            </div>
-            <div className="form__element form__span">
-                <label htmlFor="title">title:</label>
-                <input
-                    id="title"
-                    name="subject"
-                    className="form__input"
-                    type="text"
-                    value={formValue.subject}
-                    required
-                    onChange={onChange}
-                />
-            </div>
+      <div className="form__element form__span">
+        <label htmlFor="body">message:</label>
+        <textarea
+          id="body"
+          rows="13"
+          name="Contact-Message"
+          className="form__textarea"
+          type="text"
+          value={formValue["Contact-Message"]}
+          required
+          onChange={onChange}
+        />
+      </div>
 
-            <div className="form__element form__span">
-                <label htmlFor="body">message:</label>
-                <textarea
-                    id="body"
-                    rows="13"
-                    name="body"
-                    className="form__textarea"
-                    type="text"
-                    value={formValue.body}
-                    required
-                    onChange={onChange}
-                />
-            </div>
-
-            <input
-                type="button"
-                className="form__element form__span form__button"
-                value={isLoading ? "SENDING..." : "submit message"}
-                onClick={onSubmit}
-            />
-        </form>
-    );
+      <input
+        type="submit"
+        className="form__element form__span form__button"
+        value={isLoading ? "SENDING..." : "submit message"}
+      />
+    </form>
+  );
 };
 
 export default Form;
--- src/layout/about.js
+++ src/layout/about.js
@@ -7,7 +7,7 @@ const About=props=>{
             <p data-aos={"fade-left"} className="about__text">Motivated Intern offering outstanding computer science and relating with people abilities. Advanced education and training in Software development with Computer science degree. Eager to apply learned competencies in practical, business setting.</p>
             <h2 data-aos={"fade-left"} className="heading__2 about__bio-info">bio-info</h2>
             <ul data-aos={"fade-left"} className="about__list">
-                <li className="about__item"><SvgImage className="about__svg" type="icon-chevron-right"/><span>Birthday: </span> march 11<sup>th</sup>,1998</li>
+                <li className="about__item"><SvgImage className="about__svg" type="icon-chevron-right"/><span>Birthday: </span> march 11<sup>th</sup></li>
                 <li className="about__item"><SvgImage className="about__svg" type="icon-chevron-right"/> <span>phone: </span> +2349039388056</li>
                 <li className="about__item"><SvgImage className="about__svg" type="icon-chevron-right"/><span>city: </span> F.C.T abuja </li>
                 <li className="about__item"><SvgImage className="about__svg" type="icon-chevron-right"/><span>email: </span> koffokansi@gmail.com </li>
